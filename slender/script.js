ed3b81350aab966a18bc3f9532174917d6217f8f
Pizza883 <59063427+Pizza883@users.noreply.github.com>
2022-11-02 18:28:53 -0500

    Create script.js

---
+++ slender/script.js
@@ -0,0 +1,11 @@
+
+
+function openNav() {
+  
+  document.getElementById("Sidenav").style.width = "200px";
+}
+
+function closeNav() {
+  
+  document.getElementById("Sidenav").style.width = "0px";
+}
